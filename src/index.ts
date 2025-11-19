7d417b9c9444bfbb7d6acce03911e1553c0c99c5
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-11-18 21:00:08 -0500

    𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗛𝗘𝗣𝗘𝗜𝗡

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
 
+// Core
+export { Scraper } from './core/Scraper';
+export { HttpClient } from './core/HttpClient';
+export { BrowserClient } from './core/BrowserClient';
+
+// Types
+export * from './types';
+
+// Plugins
+export * from './plugins';
+
+// Utils
+export { logger, LogLevel } from './utils/logger';
+export * from './utils/helpers';
+
+// Factory function
+import { Scraper } from './core/Scraper';
+import { ScraperOptions } from './types';
+
+/**
+ * Crea una nueva instancia del scraper
+ * @param options Opciones de configuración
+ * @returns Instancia del scraper
+ */
+export function createScraper(options: ScraperOptions = {}): Scraper {
+  return new Scraper(options);
+}
+
+// Default export
+export default {
+  createScraper,
+  Scraper,
+};
